45731c13d1d2e1f982fdb2cbf14b9c3118a4e5e2
aleksandr <aleksandr.zaichko@gmail.com>
2016-04-20 21:33:36 +0300

    bla-bla

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,84 +1,90 @@
-# Change these
-server 'avz.bz.ua', port: 22, roles: [:web, :app, :db], primary: true
+set :stages, %w(staging production)
+set :stages_dir, 'config/deploy'
+set :default_stage, 'staging'
+set :rvm_ruby_string, "1.1.1@truba"
 
-set :repo_url,        'git@github.com:avzaleks/MyFirstRailsApp.git'
-set :application,     'MyFirstApp'
-set :user,            'deployer'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
 
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :bundle_gemfile,  "Gemfile"
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+require 'mina/multistage'
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rvm'
 
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+load File.expand_path('../unicorn-tasks.rb', __FILE__)
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
+set_default :keep_releases, 5
 
-  before :start, :make_dirs
+set :repository, "dfbdfbdfb"
+set :branch, 'master'
+
+set :term_mode, nil
+
+set :shared_paths, [
+    'config/secrets.yml',
+    'config/database.yml',
+    'log',
+    'public/system'
+]
+
+task :environment do
+  invoke :'rvm:use[1.1.1@truba]'
 end
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
+task setup: :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
+
+  queue! %[touch "#{deploy_to}/shared/config/application.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/application.yml'."]
+
+
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/public"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/public"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/public/system"]
 end
 
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+desc 'Deploys the current version to the server.'
+task deploy: :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'bundle:install'
+    invoke :'deploy:link_shared_paths'
+    invoke :'rails:db_create'
+    invoke :'rails:db_migrate'
+
+    # queue! "cd #{deploy_to}/current"
+    # queue! "bundle exec rake db:seed RAILS_ENV=#{:environment}"
+
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      invoke :'unicorn:restart'
+    end
+  end
+end
+
+
+desc "Seed data to the database"
+task seed: :environment do
+    queue "cd #{deploy_to}/current"
+    queue "bundle exec rake db:seed RAILS_ENV=#{default_stage}"
+
+  to :launch do
+    invoke :'unicorn:restart'
+  end
+
+end
